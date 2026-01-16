801c78e71e29c1ff719e57af541c4c6ac3fbe205
Dave Aufderheide <daufderh@hotmail.com>
2026-01-15 20:44:39 -0500

    Forgot to commit the track documentation

--- client/src/app/models/track.ts
+++ client/src/app/models/track.ts
@@ -1,5 +1,12 @@
 import { Lane } from "./lane";
 
+/**
+ * A track defines what the driver are racing on.  It has virtual things like a name
+ * and logo which are primarly just used for displaying oon the race day screen.  It
+ * also has the lane configuration which limits the number of drivers that can race
+ * at the same time and it includes the hardware connected to the track that handles
+ * everything from lap counting, to lane power and visual effects like led lights.
+ */
 export class Track {
     readonly name: string;
     readonly lanes: Lane[];
