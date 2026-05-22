b8175211a191da82deb4df70dff64aec0a61ce1f
Gerson Ferreira <gersonfer@gmail.com>
2026-05-22 10:19:09 -0300

    Add optional embedded MongoDB support

--- server/src/test/java/com/antigravity/AppTest.java
+++ server/src/test/java/com/antigravity/AppTest.java
@@ -1,9 +1,9 @@
 package com.antigravity;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 public class AppTest {
 
