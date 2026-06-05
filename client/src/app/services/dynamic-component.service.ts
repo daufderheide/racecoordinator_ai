2741bef7077927f4fa88372ede110ccb602f02c9
Dave <contact@racecoordinator.net>
2026-06-05 16:35:19 -0400

    Fixed missing dependency issue that was causing a custom raceday page to have errors opening the race director menu https://github.com/daufderheide/racecoordinator_ai/issues/318

--- client/src/app/services/dynamic-component.service.ts
+++ client/src/app/services/dynamic-component.service.ts
@@ -6,6 +6,7 @@ import {
 } from "@angular/cdk/drag-drop";
 import { ScrollingModule } from "@angular/cdk/scrolling";
 import {
+  AsyncPipe,
   CommonModule,
   DatePipe,
   DecimalPipe,
@@ -17,10 +18,12 @@ import {
 import { Component, Injectable, Type } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { RouterLink, RouterModule } from "@angular/router";
+import { LoginDialogComponent } from "@app/components/login-dialog/login-dialog.component";
 import { AboutDialogComponent } from "@app/components/shared/about-dialog/about-dialog.component";
 import { AcknowledgementModalComponent } from "@app/components/shared/acknowledgement-modal/acknowledgement-modal.component";
 import { ConfirmationModalComponent } from "@app/components/shared/confirmation-modal/confirmation-modal.component";
 import { DemoConfigModalComponent } from "@app/components/shared/demo-config-modal/demo-config-modal.component";
+import { LanguageSelectorComponent } from "@app/components/shared/language-selector/language-selector.component";
 import { ToolbarComponent } from "@app/components/shared/toolbar/toolbar.component";
 import { AvatarUrlPipe } from "@app/pipes/avatar-url.pipe";
 import { TranslatePipe } from "@app/pipes/translate.pipe";
@@ -58,6 +61,7 @@ export class DynamicComponentService {
       standalone: true,
       imports: [
         CommonModule,
+        AsyncPipe,
         NgIf,
         NgFor,
         NgClass,
@@ -80,6 +84,8 @@ export class DynamicComponentService {
         AvatarUrlPipe,
         RouterModule,
         RouterLink,
+        LoginDialogComponent,
+        LanguageSelectorComponent,
       ],
     })(DynamicComponent);
   }
