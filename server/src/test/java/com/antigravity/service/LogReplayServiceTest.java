92362a013d587abacbff26fd007460e77f1d29c3
Dave <contact@racecoordinator.net>
2026-07-19 01:18:17 -0400

    Hopefully fixed server test failures on github

--- server/src/test/java/com/antigravity/service/LogReplayServiceTest.java
+++ server/src/test/java/com/antigravity/service/LogReplayServiceTest.java
@@ -37,7 +37,6 @@ public class LogReplayServiceTest {
 
   @Test
   public void testLogReplayParsesSerialBytes() throws Exception {
-    // Write sample log file
     try (FileWriter writer = new FileWriter(tempLogFile)) {
       writer.write(
           "2026-07-18 12:12:00.001 [Thread-1] DEBUG com.antigravity.protocols.interfaces.SerialConnection - [COM3] Received: 41 34 0A\n");
@@ -65,29 +64,21 @@ public class LogReplayServiceTest {
           }
         });
 
-    // Pre-initialize so we can register the connection BEFORE it starts reading.
-    // Wait, init() instantiates AND starts. Let's make sure it's reset.
     LogReplayService.reset();
 
-    // To avoid the race condition where the thread finishes before we register,
-    // we can't easily register before init() if init() creates the instance.
-    // Let's modify LogReaderSerialConnection to register itself when LogReplayService is created?
-    // No.
-    // We can instantiate it directly, but constructor is private.
-    // Alternatively, let's make the log file have a sleep at the beginning!
-    try (FileWriter writer = new FileWriter(tempLogFile)) {
-      // Add a 100ms gap at the start
-      writer.write("2026-07-18 12:12:00.001 [Thread-1] INFO  com.antigravity.App - Server start\n");
-      writer.write(
-          "2026-07-18 12:12:00.200 [Thread-1] DEBUG com.antigravity.protocols.interfaces.SerialConnection - [COM3] Received: 41 34 0A\n");
-      writer.write(
-          "2026-07-18 12:12:00.205 [Thread-1] DEBUG com.antigravity.protocols.interfaces.SerialConnection - [COM3] Received: 42 35\n");
-    }
+    // Use reflection to instantiate and register BEFORE starting the thread
+    // This avoids race conditions on slow CI runners
+    java.lang.reflect.Constructor<LogReplayService> constructor =
+        LogReplayService.class.getDeclaredConstructor(String.class);
+    constructor.setAccessible(true);
+    replayService = constructor.newInstance(tempLogFile.getAbsolutePath());
 
-    LogReplayService.init(tempLogFile.getAbsolutePath());
-    replayService = LogReplayService.getInstance();
+    java.lang.reflect.Field instanceField = LogReplayService.class.getDeclaredField("instance");
+    instanceField.setAccessible(true);
+    instanceField.set(null, replayService);
 
     replayService.registerSerialConnection(mockConnection);
+    replayService.start();
 
     boolean completed = latch.await(2, TimeUnit.SECONDS);
     assertTrue("Did not receive expected serial events in time", completed);
